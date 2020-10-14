189b68f0f5c168a6e58948e85285364cf93be3e9
jerik0 <jcarrico1985@live.com>
2020-10-14 14:04:58 -0500

    add beginnings of site skeleton, not finished yet but wanted to push some progress.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero">
+        <div className="icons-left">
+          <a className="rhino-icon-anchor-tag nav-item-left" href="main-logo">
+            <img className="rhino-icon-top" src="/images/rhino-500.png" alt="logo-icon"/>
+          </a>
+          <h2 className="nav-item-left">Rhino</h2>
+        </div>
+        <div className="icons-right">
+          <a className="nav-item-right" href="#about">About</a>
+          <a className="nav-item-right" href="#features">Features</a>
+          <a className="nav-item-right" href="#sign-up">Sign Up</a>
+        </div>
+      </div>
+      <div className="section center">
+        <div className="hero-content">
+          <h2 className="heading-3">Bring the world to your fingertips</h2>
+          <h3 className="hero-subhead">Share & discover travel experiences with friends, family, and a community of explorers.</h3>
+          <img className="white-btn-apple-store store-button" src="/images/apple-app-store-button-white.png" alt="apple store icon" width="175"/>
+          <img className="white-btn-google-store store-button" src="/images/google-play-button-white.png" alt="google store icon" width="175"/>
+        </div>
+      </div>
     </div>
   );
 }
